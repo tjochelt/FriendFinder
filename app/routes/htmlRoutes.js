1d3dd687e0c26668d7cc575a4218c15c3ea0c200
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-12-05 22:47:05 -0600

    routes, styling

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,29 +1,10 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
-var path = require("path");
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
+const path = require("path");
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
-  });
-
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express"),
   app = express();
-// const app = express();
 
 const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("routes/apiRoutes")(app);
-require("routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 
 app.get("/", function(req, res) {
   res.send("Hello World");
