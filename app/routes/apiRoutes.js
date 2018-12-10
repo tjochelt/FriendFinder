f132487485afe8be9c3f8ba1dda76f47e9634a8a
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-12-09 23:07:04 -0600

    more working

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -6,6 +6,8 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+    // console.log(req.body);
     friendData.push(req.body);
+    res.json(true);
   });
 };
